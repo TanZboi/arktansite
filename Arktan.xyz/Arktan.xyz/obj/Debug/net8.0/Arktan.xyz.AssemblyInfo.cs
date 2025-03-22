d6660aa2b6181c8d2d2e2e6ccffaae06ffa3ac7e
TanZboi <billtansey@pm.me>
2025-03-22 19:15:44 +0000

    added art page

--- Arktan.xyz/Arktan.xyz/obj/Debug/net8.0/Arktan.xyz.AssemblyInfo.cs
+++ Arktan.xyz/Arktan.xyz/obj/Debug/net8.0/Arktan.xyz.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Arktan.xyz")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3f123a407ef80806c7f95386ba33c49a2ce20b7d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+099a995d9dfcb0e79c840819665db5c61f768540")]
 [assembly: System.Reflection.AssemblyProductAttribute("Arktan.xyz")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Arktan.xyz")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
